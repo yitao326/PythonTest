5a011d9bb2d416dcfd8d2643657dcf932b223b34
易涛 <yitao326@163.com>
2018-11-06 21:59:58 +0800

    第一次提交

--- KYB_test/capability.py
+++ KYB_test/capability.py
@@ -6,34 +6,37 @@ desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['deviceName'] = '127.0.0.1:21503'
 desired_caps['platforVersion'] = '5.1.1'
+desired_caps['automationName']='uiautomator2'
 desired_caps['appPackage'] = 'com.tal.kaoyan'
 desired_caps['appActivity'] = '.ui.activity.SplashActivity'
 
 desired_caps['app'] = r'C:\Users\Administrator\Desktop\kaoyan3.1.0.apk'
 desired_caps['noReset'] = 'True'
+desired_caps['unicodeKeyboard'] = 'True'        #
+desired_caps['resetKeyboard'] = 'True'
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 driver.implicitly_wait(2)
 
 def check_cancelBtn():
-    print("check_cancelBtn")
+    print("check cancelBtn")
 
     try:
-        cencelBtn = driver.find_element_by_id('android:id/button2')
+        cancelBtn = driver.find_element_by_id('android:id/button2')
     except NoSuchElementException:
-        print('no CancelBtn')
+        print("no cancelBtn")
     else:
         cancelBtn.click()
 
 def check_skipBtn():
-    print("check_skipBtn")
+    print("check skipBtn")
 
     try:
         skipBtn = driver.find_element_by_id('com.tal.kaoyan:id/tv_skip')
     except NoSuchElementException:
-        print('no SkipBtn')
+        print("no skipBtn")
     else:
         skipBtn.click()
 
-check_updateBtn()
+check_cancelBtn()
 check_skipBtn()
